c7bb0982e9f8634156271133695c7acbe0b3c430
Difan Zhao <dzhao@pivotal.io>
2016-12-13 10:58:58 -0800

    Add basic script for deploying with bosh2.0
    [#135745173]
    
    Signed-off-by: Jeff Schnitzer <jschnitzer@pivotal.io>

---
+++ manifest-generation/bosh-lite-deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# You may need to update the cloud config; see cf-mysql-release/bosh-lite-update-cloud-config.sh
+#
+# This template assumes that CF is not installed. If you have CF installed, get rid of the
+# no-broker.yml and no-proxy-route.yml lines.
+#
+
+gobosh -e lite -d cf-mysql deploy ~/workspace/cf-mysql-release/manifest-generation/cf-mysql-template-v2.yml \
+  -o ~/workspace/mysql-monitoring-release/manifest-generation/bosh2.0/overrides/add-monitoring-vm.yml \
+  -o ~/workspace/cf-mysql-release/manifest-generation/bosh2.0/overrides/no-broker.yml \
+  -o ~/workspace/cf-mysql-release/manifest-generation/bosh2.0/overrides/no-proxy-route.yml \
+  -l ~/workspace/cf-mysql-release/manifest-generation/bosh2.0/bosh-lite/default-vars.yml \
+  -l ~/workspace/mysql-monitoring-release/manifest-generation/bosh2.0/bosh-lite/default-vars.yml
