c193fd216c35e61a02b386bd11eb4352a7b40a73
Jeff Schnitzer <jschnitzer@pivotal.io>
2016-12-19 15:49:04 -0800

    Mysql-diag now shows repcanary status
    [#133884913]
    
    Signed-off-by: Difan Zhao <dzhao@pivotal.io>

--- manifest-generation/bosh-lite-deploy.sh
+++ manifest-generation/bosh-lite-deploy.sh
@@ -2,9 +2,6 @@
 #
 # You may need to update the cloud config; see cf-mysql-release/bosh-lite-update-cloud-config.sh
 #
-# This template assumes that CF is not installed. If you have CF installed, get rid of the
-# no-broker.yml and no-proxy-route.yml lines.
-#
 
 gobosh -e lite -d cf-mysql deploy ~/workspace/cf-mysql-release/manifest-generation/cf-mysql-template-v2.yml \
   -o ~/workspace/mysql-monitoring-release/manifest-generation/bosh2.0/overrides/add-monitoring-vm.yml \
