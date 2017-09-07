5667c71c864cc593e471aa31e9a1348c27a2606f
Sam Serrano <sserrano@pivotal.io>
2017-09-07 16:03:09 -0700

    Converting the metrics acceptance test to directly talk to Bosh
    Signed-off-by: Alex Stupakov <astupakov@pivotal.io>

--- src/acceptance_tests/metrics/mysql_event_test.go
+++ src/acceptance_tests/metrics/mysql_event_test.go
@@ -7,31 +7,113 @@ import (
 	"database/sql"
 	 _ "github.com/go-sql-driver/mysql"
 
+	boshCliDirector "github.com/cloudfoundry/bosh-cli/director"
+	boshlogger "github.com/cloudfoundry/bosh-utils/logger"
+
 	"fmt"
 	"time"
 	"os"
+	"log"
 )
 
 var _ = Describe("mysql events", func() {
-	getLeaderConnection := func() (*sql.DB, error) {
+	// connect to bosh // inject bosh director ip/env from os.Getenv
+	// maybe also inject bosh deployment
+	// grab mysql user and password from manifest // dedicated-mysql-utils has methods for this
+	// using bosh figure out the two mysql IPs
+	// run queries on each mysql IP to determine which is leader which is follower // possibly steal from adapter release
+	// use bosh to figure out instance id for leader/follower
+
+	var (
+		boshEnvironment    string
+		boshDeploymentName string
+		mysqlInstances           []boshCliDirector.Instance
+		director           boshCliDirector.Director
+		leaderInstance boshCliDirector.Instance
+		followerInstance boshCliDirector.Instance
+	)
+
+	getMysqlConnection := func(hostIp string) (*sql.DB, error) {
 		return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/replication_monitoring?parseTime=true",
-			os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), 3306))
-	}
-	getFollowerConnection := func() (*sql.DB, error) {
-		return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/replication_monitoring?parseTime=true",
-			os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_FOLLOWER_HOST"), 3306))
+			os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), hostIp, 3306))
 	}
 
-	getLeaderJob := func() (string) {
-		return os.Getenv("JOB_ID")
+	filterMysqlInstances := func (instances []boshCliDirector.Instance) ([]boshCliDirector.Instance) {
+		mysqlInstances := make([]boshCliDirector.Instance, 0)
+		for _, instance := range instances {
+			if instance.Group == "mysql" {
+				mysqlInstances = append(mysqlInstances, instance)
+			}
+		}
+		return mysqlInstances
 	}
 
-	getFollowerJob := func() (string) {
-		return os.Getenv("FOLLOWER_JOB_ID")
+	getMysqlInstancesFromDirector := func (director boshCliDirector.Director) ([]boshCliDirector.Instance){
+		deployment, err := director.FindDeployment(boshDeploymentName)
+		Expect(err).NotTo(HaveOccurred())
+
+		allInstances, err := deployment.Instances()
+		Expect(err).NotTo(HaveOccurred())
+
+		return filterMysqlInstances(allInstances)
 	}
+
+	identifyLeaderAndFollower := func(instances []boshCliDirector.Instance) (boshCliDirector.Instance, boshCliDirector.Instance) {
+		var leader, follower boshCliDirector.Instance
+		for _, instance := range instances {
+			conn, err := getMysqlConnection(instance.IPs[0])
+			Expect(err).NotTo(HaveOccurred())
+
+			rows, err := conn.Query("show slave status")
+			Expect(err).NotTo(HaveOccurred())
+
+			if rows.Next() {
+				follower = instance
+			} else {
+				leader = instance
+			}
+		}
+		return leader, follower
+	}
+
+
+
+	BeforeSuite(func() {
+		boshEnvironment = os.Getenv("BOSH_ENVIRONMENT")
+		boshDeploymentName = os.Getenv("BOSH_DEPLOYMENT")
+		boshClientName := os.Getenv("BOSH_CLIENT")
+		boshClientSecret := os.Getenv("BOSH_CLIENT_SECRET") // TODO blow up if empty
+		boshCACert := os.Getenv("BOSH_CA_CERT")
+
+		var boshLogger = boshlogger.New(boshlogger.LevelDebug, log.New(os.Stdout, "", log.LstdFlags))
+
+		config := boshCliDirector.Config{
+			Host:         boshEnvironment,
+			Port:         25555,
+			CACert: boshCACert,
+			Client:       boshClientName,
+			ClientSecret: boshClientSecret,
+		}
+		var err error
+		director, err = boshCliDirector.NewFactory(boshLogger).New(
+			config,
+			boshCliDirector.NewNoopTaskReporter(),
+			boshCliDirector.NewNoopFileReporter(),
+		)
+		Expect(err).NotTo(HaveOccurred())
+
+		mysqlInstances = getMysqlInstancesFromDirector(director)
+		Expect(len(mysqlInstances)).To(Equal(2)) // leader + follower
+
+		leaderInstance, followerInstance = identifyLeaderAndFollower(mysqlInstances)
+
+		Expect(leaderInstance).NotTo(BeNil())
+		Expect(followerInstance).NotTo(BeNil())
+		Expect(leaderInstance).NotTo(Equal(followerInstance))
+	})
 
 	It("records the timestamp at some interval on the leader node", func() {
-		conn, err := getLeaderConnection()
+		conn, err := getMysqlConnection(leaderInstance.IPs[0])
 
 		Expect(err).NotTo(HaveOccurred())
 
@@ -58,9 +140,7 @@ var _ = Describe("mysql events", func() {
 	})
 
 	It("inserts the current bosh job id on the leader node", func() {
-		conn, err := getLeaderConnection()
-
-		leaderJobId := getLeaderJob()
+		conn, err := getMysqlConnection(leaderInstance.IPs[0])
 
 		rows, err := conn.Query("select server_id from replication_monitoring.heartbeat")
 		Expect(err).NotTo(HaveOccurred())
@@ -71,7 +151,7 @@ var _ = Describe("mysql events", func() {
 		rows.Scan(&serverIdFromTable)
 		Expect(rows.Next()).To(BeFalse())
 
-		Expect(serverIdFromTable).To(Equal(leaderJobId))
+		Expect(serverIdFromTable).To(Equal(leaderInstance.ID))
 
 		Consistently(func() string {
 			rows, _ := conn.Query("select server_id from replication_monitoring.heartbeat")
@@ -82,14 +162,11 @@ var _ = Describe("mysql events", func() {
 			Expect(rows.Next()).To(BeFalse())
 
 			return newerServerIdFromTable
-		}, "6s", "1s").Should(Equal(leaderJobId))
+		}, "6s", "1s").Should(Equal(leaderInstance.ID))
 	})
 
-	// it doesn't run on the follower
 	It("only runs the event on the leader node (and replicates to the follower)", func() {
-		Expect(getFollowerJob()).NotTo(Equal(getLeaderJob()))
-
-		conn, err := getFollowerConnection()
+		conn, err := getMysqlConnection(followerInstance.IPs[0])
 
 		rows, err := conn.Query("select server_id from replication_monitoring.heartbeat")
 		Expect(err).NotTo(HaveOccurred())
@@ -100,6 +177,6 @@ var _ = Describe("mysql events", func() {
 		rows.Scan(&serverIdOnFollower)
 		Expect(rows.Next()).To(BeFalse())
 
-		Expect(serverIdOnFollower).To(Equal(getLeaderJob()))
+		Expect(serverIdOnFollower).To(Equal(leaderInstance.ID))
 	})
 })
