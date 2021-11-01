f7cdcefcae2003080dbda573f9e76dbf32c56323
David Sharp <dsharp@vmware.com>
2021-11-01 10:53:58 -0700

    linter.sh: Run `go fmt` only once
    go fmt can edit the files it examines, so running it a second time hides
    the output making it hard to diagnose what went wrong in CI.
    
    Also simplify the colorization of stdout, and reduce noise by temporarily
    turning off `set -x`
    
    Co-authored-by: David Sharp <dsharp@vmware.com>
    Co-authored-by: Kim Bassett <kbassett@vmware.com>

--- src/test-helpers/linter.sh
+++ src/test-helpers/linter.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 lint_golang() {
+  local opts="$-"
+  set +x
+  local red=$(tput setaf 1)
+  local reset=$(tput sgr0)
+
   echo "Executing 'go fmt'..."
-  if [[ -n $(go fmt ./...) ]]; then
-    # Use process substitution to print stdout from 'go fmt' in red
-    # https://serverfault.com/questions/59262/bash-print-stderr-in-red-color
-    go fmt ./... 1> >(sed $'s,.*,\e[31m&\e[m,')
+  out=$(go fmt ./...)
+  if [ -n "$out" ] ; then
+    echo "${red}${out}${reset}" # Color stdout red to stand out from "go: downloading ..." lines
+    set "${opts}"
     return 1
   fi
 
+  echo "Executing 'go vet'..."
   go vet ./...
+  set "${opts}"
 }
