29bb4324e9d622c2aecfdfad9270f218a3caa22a
Andrew Garner <garnera@vmware.com>
2023-12-21 06:53:41 -0600

    spec: bump to ginkgo/v2

--- src/spec/metrics/acceptance_suite_test.go
+++ src/spec/metrics/acceptance_suite_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/config"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
--- src/spec/templates/indicators_test.go
+++ src/spec/templates/indicators_test.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"os/exec"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"gopkg.in/yaml.v3"
 )
